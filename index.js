fd29dc72dcea9b4e4006aeb8d05b3048ef760107
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-24 16:08:04 +0900

    Add Lv1Code

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function solution(x) {
-    var answer = true;
-    let sum = 0;
-    var arr = String(x).split("");
-    for (let i = 0; i < arr.length; i++){
-        sum += parseInt(arr[i]);
-    }
-    if (x % sum === 0)
-        return answer;
-    else
-        return !answer;
-}
+var n = 118372;
 
-solution(455);
-console.log(answer);
+console.log(String(n));
+console.log(String(n).split(''));
+console.log(String(n).split('').sort());
+console.log(String(n).split('').sort().reverse());
+console.log(typeof String(n).split('').sort().reverse().join(''));
+console.log(parseInt(String(n).split('').sort((a, b) => a - b).join('')));
+
+var r = [1, 2, 7, 4, 3];
+console.log(r.sort());
+
