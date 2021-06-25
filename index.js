5accb1e115b9fdf6d4c711b2457f179c12b9ef2c
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-25 13:01:33 +0900

    Add 약수의 합

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-var n = 118372;
-
-console.log(String(n));
-console.log(String(n).split(''));
-console.log(String(n).split('').sort());
-console.log(String(n).split('').sort().reverse());
-console.log(typeof String(n).split('').sort().reverse().join(''));
-console.log(parseInt(String(n).split('').sort((a, b) => a - b).join('')));
-
-var r = [1, 2, 7, 4, 3];
-console.log(r.sort());
-
+var s = "1453";
+var arr = s.split("").every((c)=>!isNaN(c));
+console.log(arr);
