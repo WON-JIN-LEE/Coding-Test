fdf6a6f48a25007bf22bd9b733995f1b9edba88f
WON-JIN-LEE <vopgh0409@gmail.com>
2021-07-21 16:07:17 +0900

    Add Lv2Code

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function solution(nums) {
-    var halfLen = nums.length/2;
-    const {size} =new Set(nums);
-    
-    return size > halfLen ? halfLen : size;
-}
+const str = "hello world";
+str.replace('world', 'Lee');
+console.log(str);
+console.log(str.replace('world', 'Lee'));
