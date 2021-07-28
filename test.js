d461e3823f652966de5745390948ae71f323516c
WON-JIN-LEE <vopgh0409@gmail.com>
2021-07-28 17:48:07 +0900

    Add Lv2

--- test.js
+++ test.js
@@ -1,16 +1,10 @@
-function solution(s){
-    var answer = 0;
-    const arrStr = s.split("");
-    if (arrStr[0] == ")" || arrStr[arrStr.length-1] == "(" ) return false;
-    
-    for(const ele of arrStr){
-        (ele == "(") ? answer++ : answer--;
-        if (answer<0) return false;
-    };
-    return (answer === 0) ?  true : false ;
-    
+function solution(s) {
+  var answer = 0;
+  if (s[-1] == "(") return false;
+
+  for (const ele of s) {
+    ele == "(" ? answer++ : answer--;
+    if (answer < 0) return false;
+  }
+  return answer === 0 ? true : false;
 }
-
-
-console.log(solution("()))((()"));
-
