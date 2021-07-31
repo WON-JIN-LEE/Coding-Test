a083ab2aaaf0362b270a1df05e2cbe3601faeef4
WON-JIN-LEE <vopgh0409@gmail.com>
2021-07-31 23:22:14 +0900

    Add Lv2Code

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function solution(A,B){
-    A.sort((a,b) => a-b);
-    B.sort((a,b) => b-a);
+function solution(dirs) {
+    var answer = 0;
+    const move = {U:[0,1], D:[0,-1], R:[1,0], L:[-1, 0]};
     
-    return A.reduce((acc, cur,idx) => acc + cur * B[idx] , 0 );
+    let check =new Set();
+    let now=[0,0];
+    
+    for(let i =0; i<dirs.length; i++){
+        let movX = now[0] + move[dirs[i]][0];
+        let movY = now[1] + move[dirs[i]][1];
+        
+        if(movX >5|| movX < -5 || movY >5 || movY < -5) continue;
+    
+        check.add(""+now[0] + now[1]+movX+movY);
+        check.add(""+movX + movY + now[0] + now[1]);
+        
+        now = [movX, movY];
+    }
+    return check.size/2;
 }
