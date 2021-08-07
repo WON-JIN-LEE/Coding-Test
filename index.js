c86def2c4b40df40501cec6d36d0216fa0c7ee16
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-07 23:49:48 +0900

    Add Cache Code

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-function solution(s) {
-    let answer = [0, 0];
-    let str = s;
-    let len = s.length;
-
-    while (len>1) {
-        str = [...str].filter(element => '1' === element).join('');
-        let oneCount = str.length;
+function solution(cacheSize, cities) {
+    const city = cities.map(ele=> ele.toLowerCase());
+    const cityLen = city.length;
+    let time = 0;
+    let cache = [];
+    
+    if(cacheSize === 0) return 5*cityLen;
+    
+    for(let i = 0 ; i< cityLen; i++){
         
-        answer[0] += 1;
-        answer[1] += len - oneCount;
-
-        str = oneCount.toString(2);
-        len = str.length;
-
+        if(cache.includes(city[i])){
+            time +=1;
+            cache.splice(cache.indexOf(city[i]), 1);
+            cache.push(city[i]);
+        }else{
+            
+            if(cache.length === cacheSize){ cache.shift(); }
+            
+            cache.push(city[i]);
+            time +=5;
+            
         }
-    return answer;
-}
-
-function solution(s) {
-    let answer = [0, 0];
-    let str = s;
-
-    while (str.length > 1) {
-
-        answer[0] += 1;
-        answer[1] += (str.match(/0/g) || []).length;
-        str = str.replace(/0/g, "").length.toString(2);
-
     }
-    return answer;
+    
+    return time;
 }
