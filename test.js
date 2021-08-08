3c9c6d246b296aa4159edab62babfa11da205061
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-08 23:44:39 +0900

    Add Lv2Code

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
-const n = 5000;
-console.log(n.toString(2));
+function solution(n) {
+    const answer = new Array(n).fill().map((_, i )=> new Array(i+1));
+    
+    let count = 0;
+    let row = -1;
+    let col = 0;
+    
+    for(let i = n; i>0; i--){
+           for(let i = n; i > 0; i--) answer[++row][col] = ++count;
+           for(let i = n-1; i > 0; i--) answer[row][++col] = ++count;
+           for(let i = n-2; i > 0; i--) answer[--row][--col] = ++count;
+        n -= 3;
+    }
+    return answer.flat();
+}
