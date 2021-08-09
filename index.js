c5f48040d9b2106a89dcad5c0191e3c1bef8e61e
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-09 23:53:54 +0900

    Add Code

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-function solution(cacheSize, cities) {
-    const city = cities.map(ele=> ele.toLowerCase());
-    const cityLen = city.length;
-    let time = 0;
-    let cache = [];
-    
-    if(cacheSize === 0) return 5*cityLen;
-    
-    for(let i = 0 ; i< cityLen; i++){
-        
-        if(cache.includes(city[i])){
-            time +=1;
-            cache.splice(cache.indexOf(city[i]), 1);
-            cache.push(city[i]);
-        }else{
-            
-            if(cache.length === cacheSize){ cache.shift(); }
-            
-            cache.push(city[i]);
-            time +=5;
-            
-        }
-    }
-    
-    return time;
-}
+const a = ["tank","tank1" ];
+
+console.log(a[0][-2]);
