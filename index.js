1de10561a94501a47b33245c336b948212dca738
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-10 10:43:13 +0900

    Update Code

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const a = ["tank","tank1" ];
-
-console.log(a[0][-2]);
+function solution(number, k) {
+    const stack = []; // b : 최종 글자들이 저장될 스택 (숫자가 큰!)
+    for (let i = 0; i < number.length; i++) { // 모든 숫자 비교
+        const now = number[i]; // 현재 인덱스 숫자. 처음에는 그냥 push
+        
+        // 무조건 push 하고 다음 for 문에서 이전인덱스 숫자와 나랑 비교해서 현재인덱스가 이전인덱스보다 크면 pop하고 넣어줌. 제거하는 숫자인 (k)를 1 감소
+        // 제거할 숫자를 다 채웠으면 그냥 넣음
+        while (k > 0 && stack[stack.length - 1] < now) {
+            stack.pop();
+            k--;
+        }
+        stack.push(now);
+    }
+    // k가 0일 경우 스택은 그대로,
+    // but k가 남아있으면 뒤에서부터 제거해준다. (ex. 1010,2 -> 11)
+    console.log(stack);
+    
+    stack.splice(stack.length - k, k);
+    const answer = stack.join('');
+    console.log(answer);
+    
+    return answer;
+}
+solution('12333321', 3)
