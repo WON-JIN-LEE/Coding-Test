7df0893cbeacbd35da7bb73cab1337b0f14ca0c1
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-15 03:46:04 +0900

    Add Lv2

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function solution(numbers) {
-    function f(num){
-        if(num % 2 === 0) return num+1;
-    
-        const bit = "0"+ num.toString(2);
-        const index = bit.lastIndexOf("0");
-        return parseInt(`${bit.slice(0, index)}10${bit.slice(index+2)}`,2)
-    }
-    return numbers.map(ele => f(ele));
-}
+console.log("201" * "220");
+
+console.log(typeof "201");
+console.log(typeof +"201");
