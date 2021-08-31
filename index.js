b22037bcad213f2708e137fdd575ad32ba39c876
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-31 22:31:49 +0900

    Update Code

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ We want to win the first prize`.split('\n');
 
 const array = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 array.shift();
+
 const arr = array.map(x => x.split(" "));
+
 for (let i = 0; i < array.length; i++) {
     console.log( arr[i].map(x => x.split("").reverse().join("")).join(" ") );
 }
