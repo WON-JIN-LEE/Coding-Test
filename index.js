f5d735b1ef69a4a2cae4e1e002e189876ac388c9
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-02 10:21:05 +0900

    Update Code

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+// const array = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+// array.shift();
 
-console.log(parseFloat(10.01))
-console.log(parseFloat(10.00))
-console.log(parseFloat(10.000001))
+
+const array = `(())())
+(((()())() 
+(()())((()))  
+((()()(()))(((())))()   
+()()()()(()()())()
+(()((())()(`.split("\n");
+const fun = function (arr) {
+    const stack = [];
+    console.log(arr[arr.length - 1])
+    if (arr[0] === ")" || arr[arr.length - 1] === "(") return "NO";
+
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "(") { stack.push("("); }
+        else if (stack.length > 0) { stack.pop(); }
+        else { return "NO"; }
+    }
+    return stack.length ? "NO" : "YES";
+};
+
+const answer = array.reduce((sum, cur)=> sum + `${fun(cur)}\n`, "");
+
+console.log(answer);
--- test.js
+++ test.js
@@ -1,23 +1,10 @@
-function solution(relation) {
-    const answer = new Set();
-    const cols = relation[0].length;
-    const check = 1 << cols;
-
-    //유일성을 만족하는 키
-    for (let i = 1; i < check; i++) {
-        let temp = relation.map(x => x.filter((_, col) => i & (1 << col)).join(""));
-        const set = new Set(temp);
-
-        if (temp.length === set.size) answer.add(i);
-    }
-
-    // 최소성 맞는 키는 남기고 맞지 않는 키 제거
-        for (let x of answer) {
-            for (let y of answer) {
-                if (x >= y) continue;
-                if ((x & y) === x) answer.delete(y);
-                }
-            }
-    
-    return answer.size;
+const arr = '(()())((()))'
+const stack = [];
+for (let i = 0; i < arr.length; i++){
+    if (arr[i] === "(") { stack.push("("); }  
+    else if (arr[i] === ")" && stack.length > 0) { stack.pop(); }
+    else { return console.log(123) };
+    console.log(stack);
 }
+    
+console.log(stack);
