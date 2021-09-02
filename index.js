5eaee809c2712b6d8c97162d9eb230ee4498193d
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-02 23:52:26 +0900

    Add stack Code

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-// const array = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-// array.shift();
+let fs = require("fs");
+let input = fs
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n");
 
+const count = Number(input[0]);
+let stackNumber = 1;
+const stack = [];
+const result = [];
 
-const array = `(())())
-(((()())() 
-(()())((()))  
-((()()(()))(((())))()   
-()()()()(()()())()
-(()((())()(`.split("\n");
-const fun = function (arr) {
-    const stack = [];
-    console.log(arr[arr.length - 1])
-    if (arr[0] === ")" || arr[arr.length - 1] === "(") return "NO";
+for (let i = 1; i <= count; i++) {
+    let target = Number(input[i])
 
+    if (stackNumber <= target) {
+        while (stackNumber <= target) {
+            stack.push(stackNumber);
+            console.log("+");
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === "(") { stack.push("("); }
-        else if (stack.length > 0) { stack.pop(); }
-        else { return "NO"; }
+            if (stackNumber === target) {
+                stack.pop();
+                console.log("-");
+            }
+            stackNumber++;
+        }
+    }else{
+        if (stack[stack.length - 1] !== target) {
+            console.log("NO");
+            break;
+        } else {
+            stack.pop();
+            console.log("-");
+        }
     }
-    return stack.length ? "NO" : "YES";
-};
-
-const answer = array.reduce((sum, cur)=> sum + `${fun(cur)}\n`, "");
-
-console.log(answer);
+}
