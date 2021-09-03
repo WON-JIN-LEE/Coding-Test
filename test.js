23ee28e9ad1d2216972d8797eaa3ff1f544161e7
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-03 23:12:59 +0900

    Add description

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
-let cases = 0;
+function solution(s) {
+    const answer = s.split(' ').map((word) => word.split(''));
+    const len = answer.length;
 
-cases++;
-console.log(cases)
+    for (let i = 0; i < len; i++) {
+        answer[i]=answer[i].map((ele, index) => {
+                    if (index % 2 === 0) return ele.toUpperCase();
+                    else  return ele.toLowerCase();
+                }).join('');
+        }
+    
+    return answer.join(' ');
+}
