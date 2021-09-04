bf16875e28e55f9ed1b67648bbbab2ae154ed49a
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-04 19:58:24 +0900

    Add Code description

--- test.js
+++ test.js
@@ -1,13 +1,1 @@
-function solution(s) {
-    const answer = s.split(' ').map((word) => word.split(''));
-    const len = answer.length;
-
-    for (let i = 0; i < len; i++) {
-        answer[i]=answer[i].map((ele, index) => {
-                    if (index % 2 === 0) return ele.toUpperCase();
-                    else  return ele.toLowerCase();
-                }).join('');
-        }
-    
-    return answer.join(' ');
-}
+console.log(Math.floor(Math.sqrt(3)))
