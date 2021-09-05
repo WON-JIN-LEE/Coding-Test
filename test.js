70a940ef28ae5a0b872c902867e38e68bd2d0ed5
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-05 23:17:02 +0900

    Add Stack code

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log(Math.floor(Math.sqrt(3)))
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const lStack = input[0].split("");
+const rStack = [];
+
+const len = parseInt(input[1]);
+
+for (let i = 2; i < 2 + len; i++) {
+    const [cmd, value] = input[i].split(" ");
+
+    if (cmd === "L" && lStack.length) rStack.push(lStack.pop());
+    else if (cmd === "D" && rStack.length) lStack.push(rStack.pop());
+    else if (cmd === "B" && lStack.length) lStack.pop();
+    else if (cmd === "P") lStack.push(value);
+}
+
+const answer = lStack.join("") + rStack.reverse().join("");
+console.log(answer);
