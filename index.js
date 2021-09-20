3815adbc6123ac71be5f9e6a73d47e5006067e92
WON-JIN-LEE <vopgh0409@gmail.com>
2021-09-20 23:49:44 +0900

    Update MathCode

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n"); 
+
+
 // const readline = require("readline");
 
 // const rl = readline.createInterface({
@@ -15,20 +19,27 @@
 // });
 
 
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n"); 
-const input = `55+50-40`.split("\n"); 
-console.log(input);
-
+const input = `3 16`.split("\n"); 
+  solution(input);
 
 function solution(arr) {
-  const numbers = input[0].split("-").map((str) => // -를 기준으로 분리해서 분리한 문자열을 모두 탐색
-    str.split("+")
-       .map(Number)
-       .reduce((s, v) => s + v, 0) // +를 기준으로 나눠서 모두 더해준다.
-    ); // -를 기준으로 분리된 문자열의 갯수만큼 numbers 배열의 원소 개수가 된다.
-    console.log(numbers);
-  let answer = numbers.reduce((s, v,) => s - v, numbers[0]*2); // 첫번째 원소에서 나머지 모든 원소를 빼준다.
-      console.log(answer);
+    const [M, N] = arr[0].split(" ").map(Number);
+    const answer = [];
+
+    for (let i = M; i <= N; i++){
+        if (isPrime(i)) {
+            answer.push(i);
+        }
+    }
+
+    answer.forEach(ele => console.log(ele));
 }
 
-solution(input);
+
+function isPrime(num) {
+    if (num === 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++){
+        if (num % i === 0) return false;
+    }
+    return true;
+}
--- test.js
+++ test.js
@@ -1,27 +1,8 @@
-// const input =
-// require("fs").readFileSync("/dev/stdin").toString().split("\n"); 입력메세지 받음
-const input = `5
-3 1 4 3 2`.split("\n"); //입력메세지 받음
+const bigint = 1n;
+const number = 2;
 
-function solution(arr) {
-    let dp = Array.from({
-        length: 10001
-    }, () => 0);
-    const m = arr[1];
-    const numCoin = arr[2]
-        .split(" ")
-        .map(Number);
+// 숫자를 bigint로
+console.log(bigint + BigInt(number)); // 3n
 
-    dp[0] = 1;
-
-    for (let i = 0; i < arr[0]; i++) {
-        for (let x = numCoin[i]; x <= m; x++) {
-
-            dp[x] = dp[x] + dp[x - numCoin[i]];
-        }
-    }
-    console.log(dp[m]);
-
-}
-
-solution(input);
+// bigint를 숫자로
+console.log(Number(bigint) + number); // 3
