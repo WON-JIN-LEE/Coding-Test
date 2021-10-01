b6e6033227341b1a9b17b95eb0d8f36e6b06002a
WON-JIN-LEE <vopgh0409@gmail.com>
2021-10-01 15:02:37 +0900

    Add 브루트포스 Code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 // const input = require("fs").readFileSync("/dev/stdin").toString().split("\n"); 
 
+const { deflateSync } = require('zlib');
+
 
 // const readline = require("readline");
 
@@ -19,104 +21,27 @@
 // });
 
 
-const input = `5 5 3
-5 4
-5 2
-1 2
-3 4
-3 1`.split("\n");
-const strToNumArr = (str) => str.split(" ").map(Number);
-const [N, M, V] = strToNumArr(input.shift());
 
-const graph = Array.from({length : N+1}, ()=>[]);
-const visited = Array.from({ length: N + 1 }, () => false);
-let result;
-
-
-// const insertEdge = (vFront, vBack) => {
-//     let index;
-//     for (index = 0; index < graph[vFront].length; index++){
-//         if (graph[vFront][index] < vBack) {
-//             continue;
-//         }
-
-//         //간선이 양방향이기 떄문에 중복을 피하기 위해서
-//         //인접한 정점이 이미 배열에 존재하는 경우
-//         if (graph[vFront][index] === vBack) {
-//             index = null;
-//         }
-//         break;
-//     }
-//     if (index !== null) {
-//         graph[vFront].splice(index, 0, vBack);
-//     }
-// };
+const input = `4 4`.split("\n");
+    const [N, M] = input[0].split(" ").map(Number);
+    const visited = new Array(N+1).fill(false);
+    const output = [];
+    let result = '';
+    dfs(1);
+    console.log(result);
     
-const insertEdge = (vFront, vBack) => {
-        graph[vFront].push(vBack);
-};
-
-const dfsFun = (value) => {
-    if(visited[value]) {
+function dfs(cnt) {
+    if (cnt > M) {
+        result += `${output.join(" ")}\n`;
         return;
     }
 
-    visited[value] = true;
-    result.push(value);
-    graph[value].forEach((vertex) => {
-        if (!visited[vertex]) {
-            dfsFun(vertex);
-        }
-    });
-};
-
-
-
-const bfsFun = (value) => {
-    const willVisit = [value];
-    let v;
-
-    while (willVisit.length !== 0) {
-        v = willVisit.shift();
-        if (visited[v]) {
-            continue;
-        }
-    
-        visited[v] = true;
-        result.push(v);
-
-        graph[v].forEach((vertex) => {
-            if (!visited[vertex])
-                willVisit.push(vertex);
-        });
-
+    for (let i = 1; i <=N; i++){
+        if (visited[i] === true) continue;
+        visited[i] = true;
+        output.push(i);
+        dfs(cnt + 1);
+        output.pop();
+        visited[i] = false;
     }
-    
-};
-
-solution(input);
-function solution(input) {
-    
-     input.forEach((str)=>{
-        const [v1, v2] = strToNumArr(str);
-        insertEdge(v1, v2);
-        insertEdge(v2, v1);
-    });
-    console.log(graph);
-
-    graph.map(ele => ele.sort((a, b) => a - b));
-    console.log(graph);
-
-    result = []; //결과 변수 초기화
-    dfsFun(V);
-    console.log(result.join(" "));
-    
-    visited.fill(false);
-
-    result = []; //결과 변수 초기화
-    bfsFun(V);
-    console.log(result.join(" "));
-
 }
-
-
