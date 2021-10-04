c598c38023adbb34bba8b818942e2637ba57da78
WON-JIN-LEE <vopgh0409@gmail.com>
2021-10-04 18:51:42 +0900

    add N&M 수열

--- index.js
+++ index.js
@@ -21,25 +21,25 @@
 
 
 
-const input = `4 4`.split("\n");
+const input = `4 2`.split("\n");
     const [N, M] = input[0].split(" ").map(Number);
     const visited = new Array(N+1).fill(false);
     const output = [];
     let result = '';
-    dfs(1);
+    dfs(1,1);
     console.log(result);
     
-function dfs(cnt) {
+function dfs(index,cnt) {
     if (cnt > M) {
         result += `${output.join(" ")}\n`;
         return;
     }
 
-    for (let i = 1; i <=N; i++){
+    for (let i = index; i <=N; i++){
         if (visited[i] === true) continue;
         visited[i] = true;
         output.push(i);
-        dfs(cnt + 1);
+        dfs(i, cnt + 1);
         output.pop();
         visited[i] = false;
     }
