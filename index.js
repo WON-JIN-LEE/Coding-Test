437240cd96e283fb068dc6446c68348f250127c4
WON-JIN-LEE <vopgh0409@gmail.com>
2021-10-07 18:12:33 +0900

    Add Code

--- index.js
+++ index.js
@@ -21,22 +21,32 @@
 
 
 
-const input = `4 2`.split("\n");
-    const [N, M] = input[0].split(" ").map(Number);
+const input = `4 2
+9 8 7 1`.split("\n");
+const [N, M] = input[0].split(" ").map(Number);
+const numArr = input[1].split(" ");
+numArr.push(0);
+numArr.sort((a, b) => a - b);
+
+console.log(numArr);
+    const visited = new Array(N+1).fill(false);
     const output = [];
     let result = '';
-    dfs(1, 1);
+    dfs(1);
     console.log(result);
     
-function dfs(index, cnt) {
+function dfs(cnt) {
     if (cnt > M) {
         result += `${output.join(" ")}\n`;
         return;
     }
 
-    for (let i = index; i <=N; i++){
-        output.push(i);
-        dfs(i,cnt + 1);
+    for (let i = 1; i <=N; i++){
+        if (visited[i] === true) continue;
+        visited[i] = true;
+        output.push(numArr[i]);
+        dfs(cnt + 1);
         output.pop();
+        visited[i] = false;
     }
 }
