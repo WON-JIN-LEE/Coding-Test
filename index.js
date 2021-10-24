1ef62ff77dc25887fe15f4b7db48294990a4a891
WON-JIN-LEE <vopgh0409@gmail.com>
2021-10-24 22:47:54 +0900

    Add 진법

--- index.js
+++ index.js
@@ -15,33 +15,24 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `One is 1`.split("\n");
+const input = `ZZZZZ 36`.split("\n");
 
 
 const answer = solution(input);
 console.log(answer);
+
 function solution(input) {
-    const string = input[0]
-    const len = string.length;
-    let result = '';
-
-    for (let i = 0; i < len; i++) {
-
-        if (string[i] >= "A" && string[i] <= "Z") {
-            result += ROT13(string[i], "Z");
-        } else if (string[i] >= "a" && string[i] <= "z") {
-            result += ROT13(string[i], "z");
-        } else {
-            result += string[i];
-        }
-
-    }
-
-    return result;
+    const [N,B] = input[0].split(" ");
+    const answer = parseInt(N, B).toString(10);
+    return answer;
 }
-function ROT13(char, limt) {
-    if (char.charCodeAt(0) + 13 > limt.charCodeAt(0)) {
-        return String.fromCharCode(char.charCodeAt(0) + 13 - 26);
-    } else 
-        return String.fromCharCode(char.charCodeAt(0) + 13);
-    }
+
+// const answer = solution(input);
+// console.log(answer);
+
+// function solution(input) {
+//     const [N,B] = input[0].split(" ");
+//     const answer = parseInt(N, 10).toString(B).toUpperCase();
+    
+//     return answer;
+// }
