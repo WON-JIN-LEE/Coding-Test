58842779fa1a0cd7423c2b2ca995f259fa1e3884
WON-JIN-LEE <vopgh0409@gmail.com>
2021-11-03 23:52:55 +0900

    Add 암호 만들기

--- index.js
+++ index.js
@@ -15,28 +15,34 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `3
-26 40 83
-49 60 57
-13 89 99`.split("\n");
+const input = `4 6
+a t c i s w`.split("\n");
+const consonant = ['a', 'e', 'i', 'o', 'u'];
+let result = '';
+const output = [];
+const [L, C] = input[0].split(' ');
+const alpha = input[1].split(' ').sort();
 
+re_alpha(0, 0, 0, 0);
+console.log(result.trimEnd());
 
-
-const answer = solution(input);
-console.log(answer);
-
-
-function solution(input) {
-    const N = input.shift();
-    const arr = input.map(v => v.split(" ").map(Number));
-    const dp = Array.from({length:N}, ()=> []);
-    dp[0] = input[0].split(" ").map(Number);
-
-    for (let i = 1; i < N; i++) {
-        dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-        dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-        dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+function re_alpha(cnt, index, cons_count, vowel_count) {
+    // 자음, 모음 조건 충족 할때 result에 추가
+    if (cnt == L) {
+        if (cons_count >= 1 && vowel_count >= 2) {
+            result += `${output.join('')}\n`;
+        }
+        return;
     }
-    
-    return Math.min(...dp[N - 1]);
+    for (let i = index; i < C; i++) {
+        output.push(alpha[i]);
+        if (consonant.includes(alpha[i])) {
+            re_alpha(cnt + 1, i + 1, cons_count+1, vowel_count);
+        } else {
+            re_alpha(cnt + 1, i + 1, cons_count, vowel_count+1);
+        }
+        output.pop();
+    }
+    return;
 }
+
