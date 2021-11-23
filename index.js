e9610d468b7f650a4c5da8df896119b752ec4ec9
WON-JIN-LEE <vopgh0409@gmail.com>
2021-11-23 18:10:12 +0900

    add 재귀

--- index.js
+++ index.js
@@ -15,32 +15,25 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `3
-0 0 13 40 0 37
-0 0 3 0 7 4
-1 1 1 1 1 5`.split("\n");
-const N = +input.shift();
-for (let i = 0; i < N; i++) {
-    solution(input[i]);
-}
+const input = `3`.split("\n");
+const N = Number(input[0]); // 원판의 갯수
+const answer = [];
 
-function solution(input) {
-    const [x1, y1, r1, x2, y2, r2] = input.split(" ").map(Number);
-    let range = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
-
-    if (range === 0) {
-        if (r1 !== r2) console.log(0);
-        else if (r1 === r2) console.log(-1);
-    }
-    else if (range < r1 + r2) {
-        if (range + r1 < r2) console.log(0);
-        else if (range + r2 < r1) console.log(0);
-        else if (range + r2 === r1) console.log(1);
-        else if (range + r1 === r2) console.log(1);
-        else console.log(2);
-    } else if (range === r1 + r2) {
-        console.log(1);
-    } else if (range > r1 + r2) {
-        console.log(0);
+function Hanoi(num, from, to, other){
+    if (num === 1) {
+        answer.push(`${from} ${to}`);
+        return;
+    }else{
+        // 받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 곳으로 재귀적으로 이동
+        Hanoi(num - 1 , from, other, to);
+        // 맨 아래 원반을 목적지로 이동시킴
+        answer.push(`${from} ${to}`);
+        //다른 곳으로 옮겼던 원반들을 그 위에 얹음
+        Hanoi(num - 1, other, to, from);
     }
 }
+
+Hanoi(N, 1, 3, 2);
+
+console.log(answer.length);
+console.log(answer.join("\n"));
