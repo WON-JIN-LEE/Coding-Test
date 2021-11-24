f4d7a14b0c4850b6eb11481f98a9e4f0f709b9fe
WON-JIN-LEE <vopgh0409@gmail.com>
2021-11-24 19:38:52 +0900

    Add 분해합

--- index.js
+++ index.js
@@ -15,25 +15,24 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `3`.split("\n");
-const N = Number(input[0]); // 원판의 갯수
+const input = `5
+55 185
+58 183
+88 186
+60 175
+46 155`.split("\n");
+
+
+  const N = +input.shift();
+const arr = input.map(ele => ele.split(" "));
 const answer = [];
 
-function Hanoi(num, from, to, other){
-    if (num === 1) {
-        answer.push(`${from} ${to}`);
-        return;
-    }else{
-        // 받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 곳으로 재귀적으로 이동
-        Hanoi(num - 1 , from, other, to);
-        // 맨 아래 원반을 목적지로 이동시킴
-        answer.push(`${from} ${to}`);
-        //다른 곳으로 옮겼던 원반들을 그 위에 얹음
-        Hanoi(num - 1, other, to, from);
-    }
+for (let i = 0; i < N; i++){
+  const [x, y] = arr[i];
+  let count = 1;
+  for (let j = 0; j < N; j++) {
+    if (x < arr[j][0] && y < arr[j][1]) count++;
+  }
+  answer.push(count);
 }
-
-Hanoi(N, 1, 3, 2);
-
-console.log(answer.length);
-console.log(answer.join("\n"));
+console.log(answer.join(" "));
