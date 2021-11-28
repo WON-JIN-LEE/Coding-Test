dd081cb6c941fed73705bc6fb9ddb4b759003006
WON-JIN-LEE <vopgh0409@gmail.com>
2021-11-28 21:42:25 +0900

    좌표정렬하기

--- index.js
+++ index.js
@@ -15,50 +15,39 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `1
-4000`.split("\n");
+const input = `13
+but
+i
+wont
+hesitate
+no
+more
+no
+more
+it
+cannot
+wait
+im
+yours`.split("\n");
 solution(input);
+
 function solution(input) {
   const N = +input.shift();
-  const arr = input.sort((a, b) => a - b).map(Number);
+  const arr = input;
+  const answer = [];
+  arr.sort(compare);
+  arr.forEach(ele => {
+    if (!answer.includes(ele)) answer.push(ele);
+  });
+  console.log(answer.join("\n"));
+}
 
-
-  //산술평균
-  const avr = (arr.reduce((acc, cur) => acc + cur, 0) / N).toFixed(0);
-  
-  //중앙값
-  const midNum = arr[Math.floor(N / 2)];
-
-  //최빈값
-    const numObj = {};
-    for (let num of arr) {
-      if (numObj[num]) {
-        numObj[num] = numObj[num] + 1;
-      } else {
-        numObj[num] = 1;
-      }
-    }
-
-let hitMaxNum = Math.max(...Object.values(numObj));
-let hitMaxNumArr = [];
-let hitMaxNumResult = 0;
-for (let numKey in numObj) {
-  if (numObj[numKey] === hitMaxNum) {
-    hitMaxNumArr.push(numKey);
+function compare(a,b) {
+  if (a.length !== b.length) {
+    return a.length - b.length;
+  } else {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    else return 0;
   }
-  }
-  if (hitMaxNumArr.length > 1) {
-  hitMaxNumArr = hitMaxNumArr.sort((a, b) => a - b);
-  hitMaxNumResult = hitMaxNumArr[1];
-} else {
-  hitMaxNumResult = hitMaxNumArr[0];
-  }
-  
-  //범위값
- const maxSubMin = arr[arr.length - 1] - arr[0];
-  
-  console.log(avr);
-  console.log(midNum);
-  console.log(hitMaxNumResult);
-  console.log(maxSubMin);
 }
