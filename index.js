287f4313cad4c81ca8ded4ec2f76afb27bfb9b27
WON-JIN-LEE <vopgh0409@gmail.com>
2021-11-29 17:59:18 +0900

    좌표 압축

--- index.js
+++ index.js
@@ -15,39 +15,23 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `13
-but
-i
-wont
-hesitate
-no
-more
-no
-more
-it
-cannot
-wait
-im
-yours`.split("\n");
-solution(input);
+const input = `5
+2 4 -10 4 -9`.split("\n");
+
+console.log(solution(input));
 
 function solution(input) {
-  const N = +input.shift();
-  const arr = input;
+  let [n, x] = input;
+  x = x.split(" ").map(Number);
   const answer = [];
-  arr.sort(compare);
-  arr.forEach(ele => {
-    if (!answer.includes(ele)) answer.push(ele);
-  });
-  console.log(answer.join("\n"));
-}
+  const set = Array.from(new Set(x)).sort((a,b)=>a-b);
+  const object = {};
 
-function compare(a,b) {
-  if (a.length !== b.length) {
-    return a.length - b.length;
-  } else {
-    if (a < b) return -1;
-    else if (a > b) return 1;
-    else return 0;
+  set.forEach((item, idx) => object[item] = idx);
+
+  for (let i = 0; i < x.length; i++){
+    answer.push(object[x[i]]);
   }
+
+  return answer.join(" ");
 }
