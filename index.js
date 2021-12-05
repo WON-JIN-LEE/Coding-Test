0230240efd9b8513b1e28ca606f94f46a2e24bee
WON-JIN-LEE <vopgh0409@gmail.com>
2021-12-05 23:16:15 +0900

    피보나치 함수

--- index.js
+++ index.js
@@ -15,71 +15,35 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `0 3 5 4 6 9 2 7 8
-7 8 2 1 0 5 6 0 9
-0 6 0 2 7 8 1 3 5
-3 2 1 0 4 6 8 9 7
-8 0 4 9 1 3 5 0 6
-5 9 6 8 2 0 4 1 3
-9 1 7 6 5 2 0 8 0
-6 0 3 7 0 1 9 5 2
-2 5 8 3 9 4 7 6 0`.split("\n");
+const input = `2
+6
+22`.split("\n");
 
-const SIZE = 9;
-const boardMap = input.map(ele => ele.split(" ").map(Number));
-const zeroArr = findZero(boardMap);
-const N = +zeroArr.length;
-let answer = Array.from({ length :9},()=>[]);
-
-sudokuCheck(0);
-function sudokuCheck(cnt) {
-    //종료조건
-    if (cnt === N) {
-        console.log(boardMap.map(ele => ele.join(" ")).join("\n"));
-
-        process.exit(0);
-    }
-
-    let [zeroX, zeroY] = zeroArr[cnt];
-
-    for (let i = 1; i <= SIZE; i++) {
-        if (isPossible(zeroX, zeroY, i)) {
-            boardMap[zeroX][zeroY] = i;
-            sudokuCheck(cnt + 1);
-            boardMap[zeroX][zeroY] = 0;
-        }
-    }
-}
-
-
-function findZero(boradMap) {
-  const zeros = [];
-  for (let i = 0; i < SIZE; i++){
-    for (let j = 0; j <SIZE; j++) {
-      if (boradMap[i][j] === 0) zeros.push([i, j]);
-    }
+var cnt = input[0];
+var inputlist = input.slice(1);
+ 
+const countFibonacci = n => {
+  const returnObj = {
+    zeroCount: [1, 0],
+    oneCount: [0,1]
   }
-  return zeros;
-}
-
-function isPossible(x, y, value) {
-  //row check  
-  for (let i = 0; i < SIZE; i++){
-    if(boardMap[x][i] === value) return false; 
+ 
+  if (n <= 1) {
+    return returnObj  
   }
-
-  //col check  
-  for (let i = 0; i < SIZE; i++){
-    if(boardMap[i][y] === value) return false; 
+ 
+  for (let i = 2; i < n+1; i++) {
+    returnObj.zeroCount.push(returnObj.zeroCount[i-1] + returnObj.zeroCount[i-2]);
+    returnObj.oneCount.push(returnObj.oneCount[i-1] + returnObj.oneCount[i-2]);
   }
-
-  // 3 x 3 check
-  let threeX = Math.floor(x / 3) * 3;
-  let threeY = Math.floor(y / 3) * 3;
-  for (let i = threeX; i < threeX+3; i++) {
-    for (let j = threeY; j < threeY+3; j++) {
-      if (boardMap[i][j] === value) return false;
-    }
-  }
-  return true;
+ 
+  return returnObj;
+};
+ 
+// 미리 40까지의 list를 생성
+const cache = countFibonacci(Math.max(...inputlist));
+console.log(cache);
+for(let i = 0; i < cnt; i++) {
+  num = inputlist[i];
+  console.log(`${cache.zeroCount[num]} ${cache.oneCount[num]}`);
 }
