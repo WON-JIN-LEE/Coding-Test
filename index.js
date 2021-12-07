66573a4dd565ef77724d6ecdac0e3255f64944c2
WON-JIN-LEE <vopgh0409@gmail.com>
2021-12-07 22:49:01 +0900

    Add 01타일

--- index.js
+++ index.js
@@ -10,38 +10,26 @@
 // const input=[];
 // rl.on("line", function (x) {
 //     input.push(x);
-  
+
 // }).on("close", function () {
 //     solution(input);
 //   process.exit();
 // });
-const input = `1 1 1
-2 2 2
-10 4 6
-50 50 50
--1 7 18
--1 -1 -1`.split("\n");
-input.pop();
-const hash = {};
-const w = (a, b, c) => {
-  let key = `${a},${b},${c}`
+const input = `4`.split("\n");
+const MOD = 15746;
+const dp = new Array(1000001);
+dp[1] = 1;
+dp[2] = 2;
 
-     if (hash[key]) return hash[key];
-  if (a <= 0 || b <= 0 || c <= 0) return 1;
-  if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
-  if (a < b && b < c) {
-    const retval = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-    hash[key] = retval;
-    return retval;
-  }
+console.log(solution(input));
 
-  const retval = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
-  hash[key] = retval;
-  return retval;
-};
+function solution(input) {
+    const N = +input[0];
 
-
-
-
-
-console.log(w(250, 20, 20));
+    if (3 > N) return dp[N];
+    for (let i = 3; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        dp[i] %= MOD;
+    }
+    return dp[N];
+}
