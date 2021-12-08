2584a61e1ae6c9e7bf6cfb3fec3ea3409fd2ac79
WON-JIN-LEE <vopgh0409@gmail.com>
2021-12-08 23:46:25 +0900

    add dp 계단오르기, 파도반 수열

--- index.js
+++ index.js
@@ -15,21 +15,30 @@
 //     solution(input);
 //   process.exit();
 // });
-const input = `4`.split("\n");
-const MOD = 15746;
-const dp = new Array(1000001);
-dp[1] = 1;
-dp[2] = 2;
+const input = `6
+10
+20
+15
+25
+10
+20`.split("\n");
+const N = +input.shift();
+const arr = input.map(Number);
+const dp = [];
+dp[0] = arr[0];
+dp[1] = Math.max(arr[0] + arr[1], arr[1]);
+dp[2] = Math.max(arr[0] + arr[2], arr[1]+arr[2]);
 
-console.log(solution(input));
 
-function solution(input) {
-    const N = +input[0];
+solution(N, arr);
 
-    if (3 > N) return dp[N];
-    for (let i = 3; i <= N; i++) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
-        dp[i] %= MOD;
+console.log(dp[N - 1]);
+
+
+function solution(N,arr) {
+
+    for (let i = 3; i < N; i++) {
+        dp[i] = Math.max(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2]);
     }
-    return dp[N];
+    return;
 }
