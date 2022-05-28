e673bdd75044c6105bd4cea3837b6b4e339dfd65
WON-JIN-LEE <vopgh0409@gmail.com>
2022-05-28 22:54:08 +0900

    Add 배열탐색

--- BAEKJOON/그래프탐색알고리즘/섬의 크기 구하기(DFS).js
+++ BAEKJOON/그래프탐색알고리즘/섬의 크기 구하기(DFS).js
@@ -1,30 +1,5 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-// const readline = require("readline");
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// const input=[];
-// rl.on("line", function (x) {
-//     input.push(x);
-
-// }).on("close", function () {
-//     solution(input);
-//   process.exit();
-// });
-const input = `mirkovC4nizCC44
-C4`.split("\n");
-
-
-
-
-// const inp =["1101110000", "1100001000", "1000011000", "0000000000", "0000100100", "1110101111"];
-const inp = ["111", "101", "111"];
-
-
+const inp =["1110000", "0001110", "0000001",'0000011'];
 function solution(maps) {
   maps = maps.map((str) => str.split(""));
 
@@ -34,21 +9,19 @@ function solution(maps) {
   let dx = [-1, 0, 1, 0];
   let dy = [0, -1, 0, 1];
   const stack = [];
-  let size = 0;
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       if (maps[i][j] === "1") {
         stack.push([i, j]);
         
-        size = 0;
+        let temp = new Set();
 
         while (stack.length) {
           const [x, y] = stack.pop();
-          
-          if (maps[x][y] !== `1`) continue;
+
           maps[x][y] = "0";
-          size++;
+          temp.add(`${x} ${y}`)
 
 
           for (let k = 0; k < 4; k++) {
@@ -68,7 +41,7 @@ function solution(maps) {
 
           }
         }
-        answer.add(size);
+      answer.add(temp.size);
       }
     }
   }
